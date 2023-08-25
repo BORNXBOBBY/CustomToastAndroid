496977f6c07ce5497fb4e0167ff7e9ab5f34c604
BORNXBOBBY <bobbyyadav.445566@gmail.com>
2023-08-25 11:34:50 +0530

    Toast In all Directions

--- app/src/main/java/com/example/customtoast/MainActivity.kt
+++ app/src/main/java/com/example/customtoast/MainActivity.kt
@@ -12,10 +12,31 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        val btn_show_toast = findViewById<Button>(R.id.btn_show_toast)
+        val btn_show_One = findViewById<Button>(R.id.btnUp)
+        val btn_show_Two = findViewById<Button>(R.id.btnDown)
+
+        val btn_show_Three = findViewById<Button>(R.id.btnRight)
+
+        val btn_show_Four = findViewById<Button>(R.id.btnLeft)
+        val btn_show_Five = findViewById<Button>(R.id.btncenter)
+
         // apply an onClickListener() method
-        btn_show_toast.setOnClickListener{
-                Toast(this).showCustomToast ("Hello! This is a custom Toast!", this)
+        btn_show_One.setOnClickListener{
+                Toast(this).showToastOne ("Hello! This is a custom Toast!", this)
         }
+        btn_show_Two.setOnClickListener{
+            Toast(this).showToastTwo ("Hello! This is a custom Toast!", this)
+        }
+        btn_show_Three.setOnClickListener{
+            Toast(this).showToastThree ("Hello! This is a custom Toast!", this)
+        }
+        btn_show_Four.setOnClickListener{
+            Toast(this).showToastFour ("Hello! This is a custom Toast!", this)
+        }
+        btn_show_Five.setOnClickListener{
+            Toast(this).showToastFive ("Hello! This is a custom Toast!", this)
+        }
+
+
     }
 }
--- app/src/main/java/com/example/customtoast/WrapToast.kt
+++ app/src/main/java/com/example/customtoast/WrapToast.kt
@@ -1,11 +1,13 @@
 package com.example.customtoast
 
 import android.app.Activity
+
+
 import android.view.Gravity
 import android.widget.TextView
 import android.widget.Toast
 
-fun Toast.showCustomToast(message: String, activity: Activity)
+fun Toast.showToastOne(message: String, activity: Activity)
 {
     val layout = activity.layoutInflater.inflate (
         R.layout.custom_toast_layout,
@@ -18,7 +20,89 @@ fun Toast.showCustomToast(message: String, activity: Activity)
 
     // use the application extension function
     this.apply {
-        setGravity(Gravity.NO_GRAVITY, 500, 500)
+        setGravity(Gravity.TOP, 0, 0)
+
+        duration = Toast.LENGTH_LONG
+        view = layout
+        show()
+    }
+}
+
+fun Toast.showToastTwo(message: String, activity: Activity)
+{
+    val layout = activity.layoutInflater.inflate (
+        R.layout.custom_toast_layout,
+        activity.findViewById(R.id.toast_container)
+    )
+
+    // set the text of the TextView of the message
+    val textView = layout.findViewById<TextView>(R.id.toast_text)
+    textView.text = message
+
+    // use the application extension function
+    this.apply {
+        setGravity(Gravity.BOTTOM, 0, 0)
+        duration = Toast.LENGTH_LONG
+        view = layout
+        show()
+    }
+}
+
+fun Toast.showToastThree(message: String, activity: Activity)
+{
+    val layout = activity.layoutInflater.inflate (
+        R.layout.custom_toast_layout,
+        activity.findViewById(R.id.toast_container)
+    )
+
+    // set the text of the TextView of the message
+    val textView = layout.findViewById<TextView>(R.id.toast_text)
+    textView.text = message
+
+    // use the application extension function
+    this.apply {
+        setGravity(Gravity.LEFT, 0, 0)
+        duration = Toast.LENGTH_LONG
+        view = layout
+        show()
+    }
+}
+
+fun Toast.showToastFour(message: String, activity: Activity)
+{
+    val layout = activity.layoutInflater.inflate (
+        R.layout.custom_toast_layout,
+        activity.findViewById(R.id.toast_container)
+    )
+
+    // set the text of the TextView of the message
+    val textView = layout.findViewById<TextView>(R.id.toast_text)
+    textView.text = message
+
+    // use the application extension function
+    this.apply {
+        setGravity(Gravity.RIGHT, 100, -100)
+        duration = Toast.LENGTH_LONG
+        view = layout
+        show()
+    }
+}
+
+
+fun Toast.showToastFive(message: String, activity: Activity)
+{
+    val layout = activity.layoutInflater.inflate (
+        R.layout.custom_toast_layout,
+        activity.findViewById(R.id.toast_container)
+    )
+
+    // set the text of the TextView of the message
+    val textView = layout.findViewById<TextView>(R.id.toast_text)
+    textView.text = message
+
+    // use the application extension function
+    this.apply {
+        setGravity(Gravity.NO_GRAVITY, 0, 0)
         duration = Toast.LENGTH_LONG
         view = layout
         show()
